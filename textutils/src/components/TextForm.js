8f515b46af5a3cab1f1af857729595f40995ef31
Ranjit vishwakarma <vishwakarmaranjit8109@gmail.com>
2025-12-14 22:16:49 +0530

    commit

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -18,6 +18,18 @@ export default function TextForm(props) {
     setText(newText);
   }
 
+  const clearText = () =>{
+    let newText = ("");
+    setText(newText);
+  }
+
+
+   const countVowels = () => {
+    const vowels = text.match(/[aeiouAEIOU]/g);
+    const count = vowels ? vowels.length : 0;
+    alert(`Number of vowels: ${count}`);
+  };
+
   const [text, setText] = useState("");
   // text = "new text"; // Wrong way to change the state
   // setText = ("new text"); // Correct way to change the state
@@ -30,6 +42,8 @@ export default function TextForm(props) {
       </div>
       <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
       <button className="btn btn-info mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
+      <button className="btn btn-dark mx-1" onClick={clearText}>Clear Text</button>
+      <button className="btn btn-warning mx-1" onClick={countVowels}>Clear Text</button>
     </div>
     <div className="container my-3">
       <h2>Your text summary</h2>
