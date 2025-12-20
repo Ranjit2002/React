662138d6e10ccbe3376a5188870412f39bd3cc10
Ranjit vishwakarma <vishwakarmaranjit8109@gmail.com>
2025-12-20 19:49:31 +0530

    commit

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,30 +1,65 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 
 export default function TextForm(props) {
-  const handleUpClick = () =>{
+  const handleUpClick = () => {
     // console.log("UpperCase was clicked: " + text);
     let newText = text.toUpperCase()
     setText(newText);
   }
 
-  const handleOnChange = (event) =>{
+  const handleOnChange = (event) => {
     // console.log("On Change")
     setText(event.target.value);
   }
 
-  const handleLoClick = () =>{
+  const handleLoClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
   }
 
-  const clearText = () =>{
+  const clearText = () => {
     let newText = ("");
     setText(newText);
   }
 
+  const handleCopy = () => {
+    let text = document.getElementById("mybox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
 
-   const countVowels = () => {
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
+
+  // const firstLetterCapital = () => {                         // This function will make the first letter capital of a sentence 
+  //   if (!text) return;
+
+  //   const newText =
+  //     text.charAt(0).toUpperCase() + text.slice(1);
+
+  //   setText(newText);
+  // };
+
+  const firstLetterCapital = () => {
+    if (!text) return;
+
+    const newText = text
+      .split(" ")
+      .map(
+        word =>
+          word.charAt(0).toUpperCase() + word.slice(1)
+      )
+      .join(" ");
+
+    setText(newText);
+  };
+
+
+
+  const countVowels = () => {
     const vowels = text.match(/[aeiouAEIOU]/g);
     const count = vowels ? vowels.length : 0;
     alert(`Number of vowels: ${count}`);
@@ -35,23 +70,26 @@ export default function TextForm(props) {
   // setText = ("new text"); // Correct way to change the state
   return (
     <>
-    <div className="container">
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-info mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
+        <button className="btn btn-danger mx-1" onClick={clearText}>Clear Text</button>
+        <button className="btn btn-warning mx-1" onClick={countVowels}>Count Vowels</button>
+        <button className="btn btn-dark mx-1" onClick={handleCopy}>Copy Text</button>
+        <button className="btn btn-success mx-1" onClick={handleExtraSpaces}>Remove Extra Space</button>
+        <button className="btn btn-secondary mx-1" onClick={firstLetterCapital}>Make First Letter Capital</button>
       </div>
-      <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
-      <button className="btn btn-info mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
-      <button className="btn btn-dark mx-1" onClick={clearText}>Clear Text</button>
-      <button className="btn btn-warning mx-1" onClick={countVowels}>Clear Text</button>
-    </div>
-    <div className="container my-3">
-      <h2>Your text summary</h2>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
-      <p>{0.008 * text.split(" ").length} Minutes read</p>
-      <h2>Preview</h2>
-      <p>{text}</p>
-    </div>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
     </>
   );
 }
